437cd392e7fcc0b10088a307446c140bc1d3811c
fantast1czz <151417263+fantast1czz@users.noreply.github.com>
2024-03-12 15:39:45 +0000

    Add files via upload

--- client/conf/settings.py
+++ client/conf/settings.py
@@ -43,11 +43,12 @@ FILE_DIR = os.path.join(BASE_DIR, 'datas')
 
 LEVEL = 'DEBUG'
 
-# log configuration dictionary
+
+# 日志配置字典
 LOGGING_DIC = {
     'version': 1.0,
     'disable_existing_loggers': False,
-    # log format
+    # 日志格式
     'formatters': {
         'standard': {
             'format': '%(asctime)s %(threadName)s:%(thread)d [%(name)s] %(levelname)s [%(pathname)s:%(lineno)d] %(message)s',
@@ -62,59 +63,45 @@ LOGGING_DIC = {
         },
     },
     'filters': {},
-    # log handlers
+    # 日志处理器
     'handlers': {
         'console_debug_handler': {
-            'level': LEVEL,  # limit of logging handler level
-            'class': 'logging.StreamHandler',  # output into terminal
-            'formatter': 'simple'  # log configuration
+            'level': LEVEL,  # 日志处理的级别限制
+            'class': 'logging.StreamHandler',  # 输出到终端
+            'formatter': 'simple'  # 日志格式
         },
-        # 'file_info_handler': {
-        #     'level': 'INFO',
-        #     'class': 'logging.handlers.RotatingFileHandler',  # save as file, log rotating
-        #     'filename': INFO_LOG_DIR,
-        #     'maxBytes': 1024*1024*10,  # log size 10M
-        #     'backupCount': 10,  # limit the number of log files that can be saved
-        #     'encoding': 'utf-8',
-        #     'formatter': 'standard',
-        # },
-        # 'file_error_handler': {
-        #     'level': 'INFO',
-        #     'class': 'logging.handlers.RotatingFileHandler',  # save as file, log rotating
-        #     'filename': ERROR_LOG_DIR,
-        #     'maxBytes': 1024*1024*10,  # log size 10M
-        #     'backupCount': 10,  # limit the number of log files that can be saved
-        #     'encoding': 'utf-8',
-        #     'formatter': 'standard',
-        # },
-        # 'file_asyncio_handler': {
-        #     'level': 'INFO',
-        #     'class': 'logging.handlers.RotatingFileHandler',  # save as file, log rotating
-        #     'filename': ASYNCIO_ERROR_LOG_DIR,
-        #     'maxBytes': 1024*1024*10,  # log size 10M
-        #     'backupCount': 10,  # limit the number of log files that can be saved
-        #     'encoding': 'utf-8',
-        #     'formatter': 'standard',
-        # },
+        'file_info_handler': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件,日志轮转
+            'filename': INFO_LOG_DIR,
+            'maxBytes': 1024*1024*10,  # 日志大小 10M
+            'backupCount': 10,  # 日志文件保存数量限制
+            'encoding': 'utf-8',
+            'formatter': 'standard',
+        },
+        'file_error_handler': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件,日志轮转
+            'filename': ERROR_LOG_DIR,
+            'maxBytes': 1024*1024*10,  # 日志大小 10M
+            'backupCount': 10,  # 日志文件保存数量限制
+            'encoding': 'utf-8',
+            'formatter': 'standard',
+        },
     },
-    # loggers
-    # 'loggers': {
-    #     '': {  # 导入时logging.getLogger时使用的app_name
-    #         'handlers': ['console_debug_handler', 'file_info_handler'],  # 日志分配到哪个handlers中
-    #         'level': 'DEBUG',  # 日志记录的级别限制
-    #         'propagate': False,  # 默认为True，向上（更高级别的logger）传递，设置为False即可，否则会一份日志向上层层传递
-    #     },
-        # 'error_logger': {  # 导入时logging.getLogger时使用的app_name
-        #     'handlers': ['file_error_handler', 'console_debug_handler'],  # 日志分配到哪个handlers中
-        #     'level': 'ERROR',  # 日志记录的级别限制
-        #     'propagate': False,  # 默认为True，向上（更高级别的logger）传递，设置为False即可，否则会一份日志向上层层传递
-        # },
-        # 'asyncio': {  # 导入时logging.getLogger时使用的app_name
-        #     'handlers': ['file_asyncio_handler'],  # 日志分配到哪个handlers中
-        #     'level': 'ERROR',  # 日志记录的级别限制
-        #     'propagate': False,  # 默认为True，向上（更高级别的logger）传递，设置为False即可，否则会一份日志向上层层传递
-        # },
-    # }
+    # 日志记录器
+    'loggers': {
+        '': {  # 导入时logging.getLogger时使用的app_name
+            'handlers': ['console_debug_handler', 'file_info_handler'],  # 日志分配到哪个handlers中
+            'level': 'DEBUG',  # 日志记录的级别限制
+            'propagate': False,  # 默认为True，向上（更高级别的logger）传递，设置为False即可，否则会一份日志向上层层传递
+        },
+        'error_logger': {  # 导入时logging.getLogger时使用的app_name
+            'handlers': ['file_error_handler', 'console_debug_handler'],  # 日志分配到哪个handlers中
+            'level': 'ERROR',  # 日志记录的级别限制
+            'propagate': False,  # 默认为True，向上（更高级别的logger）传递，设置为False即可，否则会一份日志向上层层传递
+        },
+    }
 }
 
 logging.config.dictConfig(LOGGING_DIC)
